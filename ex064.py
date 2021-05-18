dec542c7f78104b8ef9b381776ef7a91c06ea435
André <andre.carvalho@hotmail.com.br>
2021-05-17 21:05:34 -0300

    Tratando vários valores v1.0

---
+++ ex064.py
@@ -0,0 +1,7 @@
+num = soma = cont = 0
+while num != 999:
+    num = int(input('Digite um número [999 para parar]: '))
+    if num != 999:
+        soma += num
+        cont += 1
+print(f'Você digitou {cont} números e a soma entre eles foi {soma}.')
