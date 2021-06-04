62ed2c3fa920f312fe8d9ae018a88c0f1a65a155
André <andre.carvalho@hotmail.com.br>
2021-06-03 22:43:29 -0300

    Maior e menor valores na lista

---
+++ ex078.py
@@ -0,0 +1,12 @@
+valores = []
+for v in range(0,5):
+    valores.append(int(input(f'Digite um valor para a posição {v}: ')))
+print(f'Você digitou os valores {valores}')
+print(f'O maior valor digitado foi {max(valores)} nas posições ', end='')
+for pos, v in enumerate(valores):
+    if max(valores) == v:
+        print(f'{pos}... ', end='')
+print(f'\nO menor valor digitado foi {min(valores)} nas posições ', end='')
+for pos, v in enumerate(valores):
+    if min(valores) == v:
+        print(f'{pos}... ', end='')
