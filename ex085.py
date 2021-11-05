c4735f9e11ef640f6740d71b47b4731f113f3c03
André <andre.carvalho@hotmail.com.br>
2021-11-04 22:30:53 -0300

    Ajustes na descrição dos exercícios

--- ex085.py
+++ ex085.py
@@ -1,9 +1,8 @@
-'''
-Crie um programa onde o usuário possa digitar sete valores numéricos
-e cadastre-os em uma lista única que mantenha separados os valores pares e ímpares. 
-No final, mostre os valores pares e ímpares em ordem crescente.
-Deve ser utilizado uma lista composta para resolver o exercício
-'''
+# Exercício Python 085
+# Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os em uma lista única que mantenha separados os valores pares e ímpares. 
+# No final, mostre os valores pares e ímpares em ordem crescente.
+# Deve ser utilizada uma lista composta para resolver o exercício.
+
 valores = [[], []]
 for c in range(0,7):
     numero = int(input(f'Digite o {c+1}o. valor: '))
--- ex086.py
+++ ex086.py
@@ -1,8 +1,8 @@
-'''
-Exercício Python 086: Crie um programa que declare uma matriz de dimensão 3x3 
-e preencha com valores lidos pelo teclado. No final, mostre a matriz na tela,
-com a formatação correta.
-'''
+# Exercício Python 086: 
+# Crie um programa que declare uma matriz de dimensão 3x3 
+# e preencha com valores lidos pelo teclado. No final, mostre a matriz na tela,
+# com a formatação correta.
+
 matriz = [[], [], []]
 for linha in range(0,3): 
     for coluna in range(0,3):
